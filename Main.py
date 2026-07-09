ea59537d9d8dc66952cab8dc0581af0310f60509
Rafael <2404827@escolas.anchieta.br>
2026-07-09 18:46:04 -0300

    tweaks

--- Main.py
+++ Main.py
@@ -1,13 +1,67 @@
 import json
+import random
+import subprocess
+
+def cleaning():
+    subprocess.run("cls", shell=True)
 
 with open("languages.json", "r", encoding="utf-8") as thingy:
     lang = json.load(thingy)
+
 def load():
     with open("save.json", "r", encoding="utf-8") as saves1:
         saves0 = json.load(saves1)
+        return saves0
+    
 def save(stats):
     with open("save.json", "w", encoding="utf-8") as saves2:
         json.dump(stats, saves2)
+
+def mape():
+    with open("map.json", "r", encoding="utf-8") as map1:
+        map2 = json.load(map1)
+        return map2
+
+def combat1(init_stats, enemy_id):
+    current_enemy = enemis[enemy_id]
+
+    enemyname = current_enemy["name"]
+    enemyhp = current_enemy["hp"]
+    enemymaxhp = current_enemy["maxhp"]
+    enemystr = current_enemy["stre"]
+    enemyxp = current_enemy["xpdrop"]
+
+    while enemyhp > 0 and init_stats["party"][0]["hp"] > 0:       
+        cleaning()
+        for combate in init_stats["party"]:
+            if combate["hp"] > 0 and enemyhp > 0:
+
+                print ("\n" + lang[language1]["combat"])
+                playerturn = input("> ").strip().lower()
+                if playerturn == "1":
+                    enemyhp -= combate["stre"]
+                    placeholdername = lang[language1]["enemyhit"]
+
+                elif playerturn == "2":
+                    print("\n" + magiasdaclassatualplaceholder)
+                else:
+                    print("\n" + placeholder)
+
+    if enemyhp > 0 and init_stats["party"][0]["hp"] > 0:
+            init_stats["party"][0]["hp"] -= enemystr
+            print ("\n" + lang[language1]["enemyhit"])
+    if enemyhp <= 0:
+        init_stats["party"][0]["xp"] += enemyxp
+        print("\n" + enemyxp)
+    elif init_stats["party"][0]["hp"] <= 0:
+        print("\n" + placeholderdeath)
+
+with open("enemies.json", "r", encoding="utf-8") as enemies:
+    enemis = json.load(enemies)
+
+with open ("party.json", "r", encoding="utf-8") as part:
+    parte = json.load(part)
+
 
 print ("Welcome to the game! / bienvenue á la jeux! / Bem vindo ao jogo!")
 print ("Select your language / Choisissez votre language / Escolha seu idioma")
@@ -21,23 +75,26 @@ print("\n" + lang[language1]["loadgame"] + "\n" + lang[language1]["newgame"])
 begin1 = input("> ").strip().lower()
 if begin1 == "2":
     init_stats = {
-        "stats" : {
-            "name" : "name",
-            "class" : "class",
-            "hp" : 10,
-            "mana" : 3,
-            "str" : 1,
-            "luck" : 1,
-            "inv" : "inv",
-            "xp" : 0,
-            "lvl" : 0,
-            "pts" : 10
-        }
+        "peoplerec":  0,
+        "party": [
+            {
+                "name" : "name",
+                "class" : "class",
+                "hp" : 10,
+                "mana" : 3,
+                "stre" : 1,
+                "luck" : 1,
+                "inv" : "inv",
+                "xp" : 1,
+                "lvl" : 0,
+                "pts" : 0
+            }
+        ]
     }
     print("\n" + lang[language1]["nameusr"])
     name1 = input("> ").strip()
 
-    init_stats["stats"]["name"] = name1
+    init_stats["party"][0]["name"] = name1
     placeholdername = lang[language1]["nameok"]
     actualname = placeholdername.replace("{name}", name1)
     print ("\n" + actualname)
@@ -56,11 +113,11 @@ if begin1 == "2":
         }
     
     statusclass = {
-        "Hobo" : {"hp" : 8, "mana": 7, "str": 3, "luck": 10},
-        "Magician" : {"hp" : 7, "mana": 15, "str": 2, "luck": 5},
-        "Security" : {"hp" : 10, "mana": 3, "str": 10, "luck": 3},
-        "Foreman" : {"hp" : 30, "mana": 1, "str": 4, "luck": 0},
-        "Chef" : {"hp" : 7, "mana": 15, "str": 3, "luck": 5}
+        "Hobo" : {"hp" : 8, "mana": 7, "stre": 3, "luck": 10},
+        "Magician" : {"hp" : 7, "mana": 15, "stre": 2, "luck": 5},
+        "Security" : {"hp" : 10, "mana": 3, "stre": 10, "luck": 3},
+        "Foreman" : {"hp" : 30, "mana": 1, "stre": 4, "luck": 0},
+        "Chef" : {"hp" : 7, "mana": 15, "stre": 3, "luck": 5}
     }
 
     while not classconf:
@@ -72,7 +129,7 @@ if begin1 == "2":
 
         placeholderclass = lang[language1]["classok"]
         classname = acceptedclass[class1]
-        init_stats["stats"]["class"] = classname
+        init_stats["party"][0]["class"] = classname
         actualclass = placeholderclass.replace("{class}", classname)
         print ("\n" + actualclass)
         print("\n" + lang[language1]["sure"])
@@ -83,11 +140,35 @@ if begin1 == "2":
         else:
             print("\n" + lang[language1]["classc"])
 
-    chosenclass = init_stats["stats"]["class"]
+    chosenclass = init_stats["party"][0]["class"]
     classst = statusclass[chosenclass]
 
-    init_stats["stats"]["hp"] = classst["hp"]
-    init_stats["stats"]["mana"] = classst["mana"]
-    init_stats["stats"]["str"] = classst["str"]
-    init_stats["stats"]["luck"] = classst["luck"]
+    init_stats["party"][0]["hp"] = classst["hp"]
+    init_stats["party"][0]["mana"] = classst["mana"]
+    init_stats["party"][0]["stre"] = classst["stre"]
+    init_stats["party"][0]["luck"] = classst["luck"]
     save(init_stats)
+else:
+    init_stats = load()
+    print("Game loaded.")
+playerlvl = init_stats["party"][0]["lvl"]
+map_data = mape()
+
+in_map = True
+while in_map:
+
+    print("\n" + lang[language1]["map1"])
+    mapc = input("> ")
+    
+    if mapc == "0":
+        save(init_stats)
+        print(lang[language1]["bye"])
+        exit()
+    elif mapc == "1":
+        if playerlvl >= 1:
+            print(placeholder)
+        else:
+            print("\n" + lang[language1]["lowlv"])
+    elif mapc == "2":
+        if playerlvl >= 10:
+            print("\n" + lang[language1]["inaid"])
